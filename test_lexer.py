1fb46d2c53fda48a826386aa21057a59d9b9e738
s.lukawski@student.utwente.nl <01169205@pw.edu.pl>
2024-04-06 18:13:56 +0200

    Testy do leksera dopasowane

--- test_lexer.py
+++ test_lexer.py
@@ -1,41 +1,61 @@
-from char_reader import Lexer, StringReader, TokenType, Token
+"""."""
+from char_reader import StringReader
+from lexer import Lexer
+from token_type import TokenType
+from my_token import MyToken
 
+from my_token_exceptions import MyTokenException
 
 
 def test_only_spaces():
+    """."""
     text = 10 * ' '
     r = StringReader(text)
     l = Lexer(r)
     assert l.curr_token is None
 
     for _ in range(10):
-        token = l.get_next_token()
-        assert token == Token(TokenType.END)
+        my_token = l.get_next_token()
+        assert my_token == MyToken(TokenType.END)
 
 
-def test_string_literal_Ala():
-    text = '\'Ala\''
+def test_string_literal_ala():
+    """."""
+    text = '\'ala\''
     r = StringReader(text)
     l = Lexer(r)
     assert l.curr_token is None
-    token = l.get_next_token()
-    assert token == Token(TokenType.STR_LITERAL, 'Ala')
+    my_token = l.get_next_token()
+    assert my_token == MyToken(TokenType.STR_LITERAL, 'ala')
 
-def test_strin_literal_Ala():
-    text = '\'Ala\''
+def test_strin_literal_ala():
+    """."""
+    text = '\'ala\''
     r = StringReader(text)
     l = Lexer(r)
     assert l.curr_token is None
-    token = l.get_next_token()
-    assert token == Token(TokenType.STR_LITERAL, 'Ala')
+    my_token = l.get_next_token()
+    assert my_token == MyToken(TokenType.STR_LITERAL, 'ala')
+
+def test_strin_literal_not_properly_ended():
+    """."""
+    text = '\'ala'
+    r = StringReader(text)
+    l = Lexer(r)
+    assert l.curr_token is None
+    try:
+        l.get_next_token()
+    except MyTokenException:
+        pass
 
 
 
 
 def test_keyword_null():
+    """."""
     text = 'null'
     r = StringReader(text)
     l = Lexer(r)
     assert l.curr_token is None
 
-    assert l.get_next_token() == Token(TokenType.NULL)
+    assert l.get_next_token() == MyToken(TokenType.NULL)
