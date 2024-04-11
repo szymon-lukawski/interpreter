3c5e56984df7dd4e7de3f6d67db65d6bd9194cd9
s.lukawski@student.utwente.nl <01169205@pw.edu.pl>
2024-04-12 01:32:44 +0200

    Drobny refactoring parsowania numerkow

--- lexer.py
+++ lexer.py
@@ -14,7 +14,10 @@ from my_token_exceptions import (
     ExclamationMarkError,
     IntLiteralTooBig,
     FloatLiteralTooBig,
-    InvalidCharsInNumberLiteral
+    InvalidCharsInNumberLiteral,
+    PrecidingZerosError,
+    NumberLiteralTooBig,
+    IdentifierTooLong,
 )
 
 from utils import is_identifier_body, is_value_a_keyword
@@ -27,6 +30,7 @@ class Lexer:
     STRING_ESCAPE = "\\"
     INT_LIMIT = 10**8 - 1
     FLOAT_CHAR_LIMIT = 20
+    IDENTIFIER_LEN_LIMIT = 100
 
     def __init__(self, reader: CharReader, INT_LIMIT=10**8 - 1) -> None:
         Lexer.INT_LIMIT = INT_LIMIT
@@ -185,48 +189,65 @@ class Lexer:
             self.curr_token.value = None
 
     def _parse_identifier(self, position: Tuple[int, int]):
-        # TODO Add limit to identifier length
         buffer: List[str] = []
         char = self.reader.char
         buffer.append(char)
         char = self.reader.get_next_char()
 
-        while char is not None and is_identifier_body(char):
+        while (
+            char is not None
+            and is_identifier_body(char)
+            and len(buffer) <= Lexer.IDENTIFIER_LEN_LIMIT
+        ):
             buffer.append(char)
             char = self.reader.get_next_char()
+        if len(buffer) > Lexer.IDENTIFIER_LEN_LIMIT:
+            raise IdentifierTooLong(position=position)
 
         value = "".join(buffer)
 
         self.curr_token = MyToken(TokenType.IDENTIFIER, value, position)
 
     def _parse_number(self, position: PositionType):
-        # TODO add limit to float
+        char_counter = 1
         value = int(self.reader.char)
         char = self.reader.get_next_char()
+        if value == 0 and char == "0":
+            raise PrecidingZerosError(position=position)
 
-        while char is not None and char.isdigit():
+        value = self._try_build_number(value, char_counter)
+        char = self.reader.char
+        if char != ".":
             if value > Lexer.INT_LIMIT:
                 raise IntLiteralTooBig(position)
+            self.curr_token = MyToken(TokenType.INT_LITERAL, value, position)
+            return
+
+        char = self.reader.get_next_char()
+        if char is None or not char.isdigit():
+            raise InvalidCharsInNumberLiteral(position)
+
+        int_part_len = len(str(value))
+        value = value * 10 + int(char)
+        char = self.reader.get_next_char()
+        counter = 1
+        char_counter += 1
+        value = self._try_build_number(value, char_counter)
+        total_len = len(str(value))
+        if total_len >= Lexer.FLOAT_CHAR_LIMIT:
+            raise FloatLiteralTooBig(position=position)
+        counter = total_len - int_part_len
+        self.curr_token = MyToken(
+            TokenType.FLOAT_LITERAL, value / (10**counter), position
+        )
+
+    def _try_build_number(self, value, char_counter):
+        char = self.reader.char
+        while char is not None and char.isdigit():
+            char_counter += 1
             value = value * 10 + int(char)
             char = self.reader.get_next_char()
-
-        if char == ".":
-            char = self.reader.get_next_char()
-            if char is not None and char.isdigit():
-                value = value * 10 + int(char)
-                char = self.reader.get_next_char()
-                counter = 1
-                while char is not None and char.isdigit():
-                    value = value * 10 + int(char)
-                    char = self.reader.get_next_char()
-                    counter += 1
-                self.curr_token = MyToken(
-                    TokenType.FLOAT_LITERAL, value / (10**counter), position
-                )
-            else:
-                raise InvalidCharsInNumberLiteral(position)
-        else:
-            self.curr_token = MyToken(TokenType.INT_LITERAL, value, position)
+        return value
 
     def _parse_comment(self, position: Tuple[int, int]):
         self.reader.next_char()
