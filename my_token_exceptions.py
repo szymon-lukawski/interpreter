ba4eaa7f2f5fef5b9526a6e62d2fb7e67e87824d
s.lukawski@student.utwente.nl <01169205@pw.edu.pl>
2024-04-11 23:15:03 +0200

    Dodane nowe podklasy błędów

--- my_token_exceptions.py
+++ my_token_exceptions.py
@@ -4,11 +4,22 @@ from my_token import PositionType
 
 class MyTokenException(Exception):
     """Base class for all Lexer related errors"""
-    def __init__(self, msg, position : PositionType):
-        self.message = msg if msg else "Can not tokenise this input"
+    def __init__(self, position : PositionType = (None, None
+                                                  ), msg: str = "Can not tokenise this input"):
+        self.message = msg
         self.row, self.col = position
         super().__init__()
 
     def __str__(self):
-        return f"MyTokenException: row: {self.row}, column: {self.col}, {self.message}"
+        return f"{self.__class__.__name__}: row: {self.row}, column: {self.col}, {self.__doc__}"
     
+
+
+class StringLiteralError(MyTokenException):
+    """Error when building string literal token"""
+
+class StringLiteralNotEnded(StringLiteralError):
+    """Got newline or end of text while building string literal"""
+
+class EscapingWrongChar(StringLiteralError):
+    """."""
