fcdaf9138f27898904a686318155cf1a65150353
s.lukawski@student.utwente.nl <01169205@pw.edu.pl>
2024-04-11 17:20:29 +0200

    Dodałem PositionType do wyjatkow

--- my_token_exceptions.py
+++ my_token_exceptions.py
@@ -1,8 +1,10 @@
 """Token exceptions"""
 from typing import Tuple
+from my_token import PositionType
+
 class MyTokenException(Exception):
     """Base class for all Lexer related errors"""
-    def __init__(self, msg, position : Tuple[int, int]):
+    def __init__(self, msg, position : PositionType):
         self.message = msg if msg else "Can not tokenise this input"
         self.row, self.col = position
         super().__init__()
