f9c918e28eebab30142e94743b324793f0242d76
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-04-22 12:09:49 +0200

    CharReader korzysta z ABC i abstractmethod, StringReader korzysta z StringIO

--- char_reader.py
+++ char_reader.py
@@ -1,6 +1,7 @@
-"""."""
+from abc import ABC, abstractmethod
+from io import StringIO
 
-class CharReader:
+class CharReader(ABC):
     """
     Abstract class for reading source char by char
     while tracking position of the current character
@@ -8,48 +9,46 @@ class CharReader:
 
     def __init__(self):
         self.char: str
-        self._row : int = 1
-        self._col : int = 0
+        self._row: int = 1
+        self._col: int = 1
 
     def next_char(self):
         """Advances to next char in source"""
-        self._next_char()
         self._update_position()
-
+        self._next_char()
 
 
     def get_position(self):
         """Returns position of current char in source"""
         return self._row, self._col
 
+    @abstractmethod
     def _next_char(self):
-        return ""
+        pass
     
     def _update_position(self):
+        if self.char == '':
+            return
         if self.char == '\n':
-            self._col = 0
-            self._row +=1
+            self._col = 1
+            self._row += 1
         else:
             self._col += 1
 
     def get_next_char(self):
         """Advances one char and returns it"""
-        self._next_char()
-        self._update_position()
+        self.next_char()
         return self.char
 
 
 class StringReader(CharReader):
     """CharReader that source is text python's string literal"""
 
-    def __init__(self, string_literal: str):
+    def __init__(self, string_literal: StringIO):
         super().__init__()
-        self.string_literal = string_literal
-        self.idx = 0
+        self.string_io = string_literal
+        self.string_io.seek(0)
+        self._next_char()
 
     def _next_char(self):
-        if self.idx < len(self.string_literal):
-            self.char = self.string_literal[self.idx]
-            self.idx += 1
-            return
-        self.char = None
+        self.char = self.string_io.read(1)
