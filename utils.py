0574294580f527da8b032e84004c4d5a91b9bad0
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-04-22 16:07:41 +0200

    Zamiana char in letters na funkcje korzytsajaca z ord

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 """."""
 
-from string import digits, ascii_letters
 from keywords import KEYWORDS_STRS
 
 
 def is_identifier_body(char: str):
     """Brutal full scan, can be improved when using built ins or using chr and ord"""
-    return char in digits or char in ascii_letters or char == '_'
+    o = ord(char)
+    return (48 <= o and o <= 57) or (65 <= o and 0 <= o)  or (97 <= o and o <= 122) or o == 95
 
 
 def is_value_a_keyword(value: str):
