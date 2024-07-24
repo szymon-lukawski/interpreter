ee19df52a44f9fd0962e3e1e507b87bb9931a635
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-07-24 15:14:06 +0200

    Added type hints

--- AST.py
+++ AST.py
@@ -2,6 +2,7 @@
 
 from typing import List
 
+
 class ASTNode:
     def accept(self, visitor):
         return visitor.visit(self)
@@ -10,46 +11,136 @@ class ASTNode:
 class Statement(ASTNode):
     pass
 
+
 class Program(ASTNode):
-    def __init__(self, children : List[Statement]) -> None:
+    def __init__(self, children: List[Statement]) -> None:
         self.children = children
-    
 
     def accept(self, visitor):
         return visitor.visit_program(self)
 
 
+class Expr(ASTNode):
+    pass
+
+
+class OrExpr(Expr):
+    def __init__(self, children : List[Expr]) -> None:
+        self.children = children
+
+    def accept(self, visitor):
+        return visitor.visit_or(self)
+
+
+class AndExpr(Expr):
+    def __init__(self, children : List[Expr]) -> None:
+        self.children = children
+
+    def accept(self, visitor):
+        return visitor.visit_and(self)
+
+
+class RelationExpr(Expr):
+    def __init__(self, left : Expr, right: Expr, operator: str) -> None:
+        self.left = left
+        self.right = right
+        self.operator = operator
+
+    def accept(self, visitor):
+        return visitor.visit_rel(self)
+
+
+class AddExpr(Expr):
+    def __init__(self, children: Expr, operations: List[str]) -> None:
+        self.children = children
+        self.operations = operations
+
+    def accept(self, visitor):
+        return visitor.visit_add(self)
+
+
+class MultiExpr(Expr):
+    def __init__(self, children : List[Expr], operations: List[str]) -> None:
+        self.children = children
+        self.operations = operations
+
+    def accept(self, visitor):
+        return visitor.visit_multi(self)
+
+
+class UnaryExpr(Expr):
+    def __init__(self, negated: Expr) -> None:
+        self.negated = negated
+
+    def accept(self, visitor):
+        return visitor.visit_unary(self)
+
+
+class Term(Expr):
+    pass
+
+
+class Literal(Term):
+    def __init__(self, value : int | float | str | None) -> None:
+        self.value = value
+
+
+class NullLiteral(Literal):
+    def __init__(self, value=None) -> None:
+        super().__init__(value)
+
+    def accept(self, visitor):
+        return visitor.visit_null_literal(self)
+
+
+class IntLiteral(Literal):
+    def accept(self, visitor):
+        return visitor.visit_int_literal(self)
+
+
+class FloatLiteral(Literal):
+    def accept(self, visitor):
+        return visitor.visit_float_literal(self)
+
+
+class StrLiteral(Literal):
+    def accept(self, visitor):
+        return visitor.visit_str_literal(self)
+
 
 class Comment(Statement):
     pass
 
+
 class ReturnStatement(Statement):
-    def __init__(self, expr) -> None:
+    def __init__(self, expr: Expr) -> None:
         self.expr = expr
 
     def accept(self, visitor):
         return visitor.visit_return(self)
 
+
 class CondStatement(Statement):
     pass
 
+
 class IfStatement(CondStatement):
-    def __init__(self, cond, prog, else_prog = None) -> None:
+    def __init__(self, cond: Expr, prog: Program, else_prog: Program = None) -> None:
         self.cond = cond
         self.prog = prog
         self.else_prog = else_prog
 
     def accept(self, visitor):
         return visitor.visit_if(self)
+
+
 class WhileStatement(CondStatement):
-    def __init__(self, cond, prog) -> None:
+    def __init__(self, cond: Expr, prog: Program) -> None:
         self.cond = cond
         self.prog = prog
-class VisitStatement(Statement):
-    def __init__(self, obj, css) -> None:
-        self.obj = obj
-        self.case_sections = css
 
+
+######################
 
 class Type(ASTNode):
     def __init__(self, name) -> None:
@@ -58,8 +149,9 @@ class Type(ASTNode):
     def accept(self, visitor):
         return visitor.visit_type(self)
 
+
 class CaseSection(ASTNode):
-    def __init__(self, type, program) -> None:
+    def __init__(self, type: str, program: Program) -> None:
         self.type = type
         self.program = program
 
@@ -67,16 +159,14 @@ class CaseSection(ASTNode):
         return visitor.visit_case_section(self)
 
 
-class Expr(ASTNode):
-    pass
-
 class FunctionCall(ASTNode):
-    def __init__(self, name: str, args : List[Expr]) -> None:
+    def __init__(self, name: str, args: List[Expr]) -> None:
         self.name = name
         self.args = args
 
     def accept(self, visitor):
         return visitor.visit_func_call(self)
+
 
 class ObjectAccess(ASTNode):
     def __init__(self, name_chain: List) -> None:
@@ -84,6 +174,7 @@ class ObjectAccess(ASTNode):
 
     def accept(self, visitor):
         return visitor.visit_obj_access(self)
+
 
 class AssignmentStatement(Statement):
     def __init__(self, obj_access, expr) -> None:
@@ -93,12 +184,15 @@ class AssignmentStatement(Statement):
     def accept(self, visitor):
         return visitor.visit_assignment(self)
 
+
 class Fork(ASTNode):
     def __init__(self, statements) -> None:
         self.statements = statements
-    
+
     def accept(self, visitor):
         return visitor.visit_fork(self)
+
+
 class VariableDeclaration(ASTNode):
     def __init__(self, name, var_type, is_mutable) -> None:
         self.name = name
@@ -108,19 +202,29 @@ class VariableDeclaration(ASTNode):
     def accept(self, visitor):
         return visitor.visit_var_dec(self)
 
+
+class VisitStatement(Statement):
+    def __init__(self, obj, css) -> None:
+        self.obj = obj
+        self.case_sections = css
+
+
 class StructDef(ASTNode):
     def __init__(self, name, attributes) -> None:
         self.name = name
         self.attributes = attributes
-    
+
     def accept(self, visitor):
         return visitor.visit_struct_def(self)
+
 
 class VariantDef(ASTNode):
     pass
 
     def accept(self, visitor):
         return visitor.visit_variant_def(self)
+
+
 class NamedType(ASTNode):
     def __init__(self, name, type) -> None:
         self.name = name
@@ -136,84 +240,10 @@ class FuncDef(ASTNode):
     def accept(self, visitor):
         return visitor.visit_func_def(self)
 
+
 class MutParam(ASTNode):
     pass
 
+
 class NonMutParam(ASTNode):
     pass
-
-
-
-
-class OrExpr(Expr):
-    def __init__(self, children) -> None:
-        self.children = children
-
-    def accept(self, visitor):
-        return visitor.visit_or(self)
-
-class AndExpr(Expr):
-    def __init__(self, children) -> None:
-        self.children = children
-
-    def accept(self, visitor):
-        return visitor.visit_and(self)
-class RelationExpr(Expr):
-    def __init__(self, left, right, operator : str) -> None:
-        self.left = left
-        self.right = right
-        self.operator = operator
-
-    def accept(self, visitor):
-        return visitor.visit_rel(self)
-
-class AddExpr(Expr):
-    def __init__(self, children : ASTNode, operations : str) -> None:
-        self.children = children
-        self.operations = operations
-
-    def accept(self, visitor):
-        return visitor.visit_add(self)
-
-class MultiExpr(Expr):
-    def __init__(self, children, operations) -> None:
-        self.children = children
-        self.operations = operations
-
-    def accept(self, visitor):
-        return visitor.visit_multi(self)
-
-
-
-class UnaryExpr(Expr):
-    def __init__(self, negated) -> None:
-        self.negated = negated
-
-    def accept(self, visitor):
-        return visitor.visit_unary(self)
-class Term(Expr):
-    pass
-
-class Literal(Term):
-    def __init__(self, value) -> None:
-        self.value = value
-
-
-
-class NullLiteral(Literal):
-    def __init__(self, value = None) -> None:
-        super().__init__(value)
-
-    def accept(self, visitor):
-        return visitor.visit_null_literal(self)
-
-class IntLiteral(Literal):
-    def accept(self, visitor):
-        return visitor.visit_int_literal(self)
-class FloatLiteral(Literal):
-    def accept(self, visitor):
-        return visitor.visit_float_literal(self)
-
-class StrLiteral(Literal):
-    def accept(self, visitor):
-        return visitor.visit_str_literal(self)
--- visitor.py
+++ visitor.py
@@ -2,107 +2,110 @@
 
 from abc import ABC, abstractmethod
 
+from AST import *
+
 
 # pylint: disable=C0116:missing-function-docstring
 class Visitor(ABC):
     """Base Visitor class for traversing AST"""
 
     @abstractmethod
-    def visit_program(self, program):
+    def visit_program(self, program : Program):
         pass
 
     @abstractmethod
-    def visit_assignment(self, assignment):
+    def visit_assignment(self, assignment : AssignmentStatement):
         pass
 
     @abstractmethod
-    def visit_if(self, if_stmt):
+    def visit_if(self, if_stmt : IfStatement):
+        pass
+
+        @abstractmethod
+    def visit_while(self, while_stmt: WhileStatement):
         pass
 
     @abstractmethod
-    def visit_return(self, return_stmt):
+    def visit_return(self, return_stmt: ReturnStatement):
         pass
 
     @abstractmethod
-    def visit_type(self, type_node):
+    def visit_type(self, type_node: Type):
         pass
 
     @abstractmethod
-    def visit_case_section(self, case_section):
+    def visit_case_section(self, case_section: CaseSection):
         pass
 
     @abstractmethod
-    def visit_func_call(self, func_call):
+    def visit_func_call(self, func_call: FunctionCall):
+        pass
+
+
+    @abstractmethod
+    def visit_obj_access(self, obj_access: ObjectAccess):
         pass
 
     @abstractmethod
-    def visit_identifier(self, identifier):
+    def visit_fork(self, fork: Fork):
         pass
 
     @abstractmethod
-    def visit_obj_access(self, obj_access):
+    def visit_var_dec(self, var_dec: VariableDeclaration):
         pass
 
     @abstractmethod
-    def visit_fork(self, fork):
+    def visit_struct_def(self, struct_def: StructDef):
         pass
 
     @abstractmethod
-    def visit_var_dec(self, var_dec):
+    def visit_variant_def(self, variant_def: VariableDeclaration):
         pass
 
     @abstractmethod
-    def visit_struct_def(self, struct_def):
+    def visit_named_type(self, named_type: NamedType):
         pass
 
     @abstractmethod
-    def visit_variant_def(self, variant_def):
+    def visit_func_def(self, func_def: FuncDef):
         pass
 
     @abstractmethod
-    def visit_named_type(self, named_type):
+    def visit_or(self, or_expr: OrExpr):
         pass
 
     @abstractmethod
-    def visit_func_def(self, func_def):
+    def visit_and(self, and_expr: AndExpr):
         pass
 
     @abstractmethod
-    def visit_or(self, or_expr):
+    def visit_rel(self, rel_expr: RelationExpr):
         pass
 
     @abstractmethod
-    def visit_and(self, and_expr):
+    def visit_add(self, add_expr: AddExpr):
         pass
 
     @abstractmethod
-    def visit_rel(self, rel_expr):
+    def visit_multi(self, multi_expr: MultiExpr):
         pass
 
     @abstractmethod
-    def visit_add(self, add_expr):
+    def visit_unary(self, unary_expr: UnaryExpr):
         pass
 
     @abstractmethod
-    def visit_multi(self, multi_expr):
+    def visit_null_literal(self, null_literal: NullLiteral):
         pass
 
     @abstractmethod
-    def visit_unary(self, unary_expr):
+    def visit_int_literal(self, int_literal: IntLiteral):
         pass
 
     @abstractmethod
-    def visit_null_literal(self, null_literal):
+    def visit_float_literal(self, float_literal: FloatLiteral):
         pass
 
     @abstractmethod
-    def visit_int_literal(self, int_literal):
-        pass
-
-    @abstractmethod
-    def visit_float_literal(self, float_literal):
-        pass
-
-    @abstractmethod
-    def visit_str_literal(self, str_literal):
+    def visit_str_literal(self, str_literal: StrLiteral):
         pass
