3d78b05ece67e0972bc3a10e87b643c4a3bec0b5
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-07-28 14:38:25 +0200

    Printer is for Generating from AST object to string object that evaluated is  eqivalent AST object, PrettyPrinter is for Creating human readable code of AST-future use for formatter.

--- pretty_printer.py
+++ pretty_printer.py
@@ -1,12 +1,17 @@
-"""Module for printing AST using visitor base class"""
+"""Module that provides class to turn AST into formatted human readable code"""
 
+from typing import Callable
 from visitor import Visitor
-from AST import *
+from AST import * 
 
 
-class Printer(Visitor):
+class PrettyPrinter(Visitor):
     def __init__(self) -> None:
         self.parts = []
+        self.indentation_multiplier = 4
+        self.space_multiplier = 1
+        self.curr_alligment = 0
+        self.curr_indentation = 0
 
     def print(self, ast : ASTNode):
         self.parts = []
@@ -14,43 +19,33 @@ class Printer(Visitor):
         return ''.join(self.parts)
     
     def visit_int_literal(self, int_literal: IntLiteral):
-        self.parts.append(f"IntLiteral({int_literal.value})")
+        self._add(str(int_literal.value))
 
     def visit_float_literal(self, float_literal: FloatLiteral):
-        self.parts.append(f"FloatLiteral({float_literal.value})")
+        self._add(str(float_literal.value))
 
     def visit_str_literal(self, str_literal: StrLiteral):
-        self.parts.append(f"StrLiteral('{str_literal.value}')")
+        self._add(f"'{str_literal.value}'")
 
     def visit_null_literal(self, null_literal: NullLiteral):
-        self.parts.append(f"NullLiteral()")
+        self._add('null')
     
     def visit_unary(self, unary_expr: UnaryExpr):
-        self.parts.append("UnaryExpr(")
+        self._add("-")
         unary_expr.negated.accept(self)
-        self.parts.append(")")
 
     def visit_add(self, add_expr : AddExpr):
-        self._visit_additive_or_multiplicative(add_expr, True)
+        self._visit_additive_or_multiplicative(add_expr)
 
     def visit_multi(self, multi_expr: MultiExpr):
-        self._visit_additive_or_multiplicative(multi_expr, False)
+        self._visit_additive_or_multiplicative(multi_expr)
         
-    def _visit_additive_or_multiplicative(self, expr: MultiExpr | AddExpr,  is_additive: bool):
-        if is_additive:
-            self.parts.append("AddExpr([")
-        else:
-            self.parts.append("MultiExpr([")
-        for i, child in enumerate(expr.children, start=1):
+    def _visit_additive_or_multiplicative(self, expr: MultiExpr | AddExpr):
+        # TODO add brackets when needed
+        for i, child in enumerate(expr.children):
             child.accept(self)
-            if i != len(expr.children):
-                self.parts.append(", ")
-        self.parts.append("], [")
-        for i, operation in enumerate(expr.operations, start=1):
-            self.parts.append(f"'{operation}'")
-            if i != len(expr.operations):
-                self.parts.append(", ")
-        self.parts.append("])")
+            if i != len(expr.children)-1:
+                self._add(f"{self.space_multiplier*" "}{expr.operations[i]}{self.space_multiplier*" "}")
 
     def visit_and(self, and_expr: AndExpr):
         self._visit_logical(and_expr, True)
@@ -59,128 +54,159 @@ class Printer(Visitor):
         self._visit_logical(or_expr, False)
 
     def _visit_logical(self, expr: AndExpr | OrExpr, is_and: bool):
-        if is_and:
-            self.parts.append("AndExpr([")
-        else:
-            self.parts.append("OrExpr([")
-        for i, child in enumerate(expr.children, start=1):
+        for i, child in enumerate(expr.children):
             child.accept(self)
-            if i != len(expr.children):
-                self.parts.append(", ")
-        self.parts.append("])")
+            if i != len(expr.children) - 1:
+                self._add(f"{self.space_multiplier*" "}{'&' if is_and else '|'}{self.space_multiplier*" "}")
 
     def visit_rel(self, rel_expr: RelationExpr):
-        self.parts.append("RelationExpr(")
         rel_expr.left.accept(self)
-        self.parts.append(", ")
+        self._add(f"{self.space_multiplier*' '}{rel_expr.operator}{self.space_multiplier*' '}")
         rel_expr.right.accept(self)
-        self.parts.append(f", '{rel_expr.operator}')")
+        
 
         
     def visit_obj_access(self, obj_access: ObjectAccess):
-        self.parts.append(f"ObjectAccess([{", ".join([f"'{name}'" for name in obj_access.name_chain])}])")
+        self._add(".".join(obj_access.name_chain))
 
 
     def visit_var_dec(self, var_dec: VariableDeclaration):
-        self.parts.append(f"VariableDeclaration('{var_dec.name}', '{var_dec.type}', {var_dec.is_mutable}")
-        if var_dec.default_value:
-            self.parts.append(", ")
-            var_dec.default_value.accept(self)
-        self.parts.append(")")
-
+        self._add_indented("")
+        self._visit_start_var_dec_or_param(var_dec)
+        self._add(';\n')
 
     def visit_assignment(self, assignment: AssignmentStatement):
-        self.parts.append("AssignmentStatement(")
+        self._add_indented("")
         assignment.obj_access.accept(self)
-        self.parts.append(", ")
+        self._add(f"{self.space_multiplier*" "}={self.space_multiplier*" "}")
         assignment.expr.accept(self)
-        self.parts.append(")")
+        self._add(';\n')
     
     
     def visit_if(self, if_stmt: IfStatement):
-        self.parts.append("IfStatement(")
+        self._add_indented(f"if{self.space_multiplier*" "}")
         if_stmt.cond.accept(self)
-        self.parts.append(", ")
-        if_stmt.prog.accept(self)
+        self._add("\n")
+        self._add_block(self._if_content, if_stmt.prog)
         if if_stmt.else_prog:
-            self.parts.append(", ")
-            if_stmt.else_prog.accept(self)
-        self.parts.append(")")
+            self._add_indented("else\n")
+            self._add_block(self._else_content, if_stmt.else_prog)
+
+    def _if_content(self, prog: Program):
+        prog.accept(self)
+
+    def _else_content(self, else_prog: Program):
+        else_prog.accept(self)
 
     def visit_while(self, while_stmt: WhileStatement):
-        self.parts.append("WhileStatement(")
+        self._add_indented(f"while{self.space_multiplier*" "}")
         while_stmt.cond.accept(self)
-        self.parts.append(", ")
+        self._add('\n')
+        self._add_indented("begin\n")
+        self.curr_indentation += 1
         while_stmt.prog.accept(self)
-        self.parts.append(")")
+        self.curr_indentation -= 1
+        self._add_indented("end\n")
+        
 
     def visit_program(self, program: Program):
-        self._visit_titled_list("Program", program.children)
-
-    def _visit_titled_list(self, title : str, list_: List[Statement]):
-        self.parts.append(f"{title}(")
-        self._visit_list(list_)
-        self.parts.append(")")
-
-    def _visit_list(self, list_: List[Statement]):
-        self.parts.append("[")
-        for i, element in enumerate(list_, start=1):
-            element.accept(self)
-            if i != len(list_):
-                self.parts.append(", ")
-        self.parts.append("]")
+        for statement in program.children:
+            statement.accept(self)
 
 
     def visit_variant_def(self, variant_def : VariantDef):
-        self.parts.append(f"VariantDef('{variant_def.name}', ")
-        self._visit_list(variant_def.named_types)
-        self.parts.append(")")
+        self._add_indented(f"{variant_def.name}{self.space_multiplier*' '}:{self.space_multiplier*' '}variant\n")
+        self._add_indented("begin\n")
+        self.curr_indentation += 1
+        for named_type in variant_def.named_types:
+            named_type.accept(self)
+        self.curr_indentation -= 1
+        self._add_indented("end\n")
 
         
     def visit_named_type(self, named_type : NamedType):
-        self.parts.append(f"NamedType('{named_type.name}', '{named_type.type}')")
+        self._add_indented(f"{named_type.name}{self.space_multiplier*' '}:{self.space_multiplier*' '}{named_type.type};\n")
     
 
 
     def visit_case_section(self, case_section : CaseSection):
-        self.parts.append(f"CaseSection('{case_section.type}', ")
+        self._add_indented(f"case{self.space_multiplier*' '}{case_section.type}\n")
+        self._add_indented('begin\n')
+        self.curr_indentation += 1
         case_section.program.accept(self)
-        self.parts.append(")")
+        self.curr_indentation -= 1
+        self._add_indented("end\n")
     
     def visit_visit(self, visit_statement: VisitStatement):
-        self.parts.append("VisitStatement(")
+        self._add_indented(f"visit{self.space_multiplier*' '}")
         visit_statement.obj.accept(self)
-        self.parts.append(", ")
-        self._visit_list(visit_statement.case_sections)
-        self.parts.append(")")
+        self._add('\n')
+        self._add_indented("begin\n")
+        self.curr_indentation += 1
+        for case_section in visit_statement.case_sections:
+            case_section.accept(self)
+        self.curr_indentation -= 1
+        self._add_indented("end\n")
     
     def visit_func_call(self, func_call: FunctionCall):
-        self.parts.append(f"FunctionCall('{func_call.name}', ")
-        self._visit_list(func_call.args)
-        self.parts.append(")")
+        self._add(f"{func_call.name}(")
+        for i, arg in enumerate(func_call.args, start=1):
+            arg.accept(self)
+            if i != len(func_call.args):
+                self._add(f',{self.space_multiplier*" "}')
+        self._add(")")
+
+    def _visit_start_var_dec_or_param(self, var_or_param : VariableDeclaration | Param):
+        self._add(f"{var_or_param.name}{self.space_multiplier*' '}:{self.space_multiplier*' '}{f"mut{self.space_multiplier*' '}" if var_or_param.is_mutable else ""}{var_or_param.type}")
+        if var_or_param.default_value:
+            self._add(f"{self.space_multiplier*" "}={self.space_multiplier*" "}")
+            var_or_param.default_value.accept(self)
+
+    def visit_param(self, param : Param):
+        self._visit_start_var_dec_or_param(param)
+
+        
     
     def visit_func_def(self, func_def : FuncDef):
-        self.parts.append(f"FuncDef('{func_def.name}', ")
-        self._visit_list(func_def.params)
-        self.parts.append(f", '{func_def.type}', ")
+        self._add_indented(f"{func_def.name}(")
+        for i, param in enumerate(func_def.params, start=1):
+            param.accept(self)
+            if i != len(func_def.params):
+                self._add(f',{self.space_multiplier*" "}')
+        self._add(f"){self.space_multiplier*' '}:{self.space_multiplier*' '}{func_def.type}\n")
+        self._add_indented("begin\n")
+        self.curr_indentation += 1
         func_def.prog.accept(self)
-        self.parts.append(")")
+        self.curr_indentation -= 1
+        self._add_indented("end\n")
     
 
     
     def visit_return(self, return_stmt: ReturnStatement):
-        self.parts.append("ReturnStatement(")
+        self._add_indented(f"return{self.space_multiplier*' '}")
         return_stmt.expr.accept(self)
-        self.parts.append(")")
+        self._add(";\n")
 
     
     
     def visit_struct_def(self, struct_def: StructDef):
-        self.parts.append(f"StructDef('{struct_def.name}', ")
-        self._visit_list(struct_def.attributes)
-        self.parts.append(")")
-    
-    
+        self._add_indented(f"{struct_def.name}{self.space_multiplier*' '}:{self.space_multiplier*' '}struct\n")
+        self._add_block(self._struct_def_content, struct_def.attributes)
+
+    def _struct_def_content(self, attrs : List[VariableDeclaration]):
+        for attr in attrs:
+            attr.accept(self)
+
+    def _add_block(self, content : Callable, args):
+        self._add_indented("begin\n")
+        self.curr_indentation += 1
+        content(args)
+        self.curr_indentation -= 1
+        self._add_indented("end\n")
 
     
-        
+    def _add_indented(self, string : str):
+        self.parts.append(f"{self.curr_indentation*self.indentation_multiplier*' '}{string}")
+
+    def _add(self, string : str):
+        self.parts.append(string)
