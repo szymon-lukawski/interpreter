4e74e599c48e64f621f7fc7f1dae67a464642448
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-08-01 15:13:18 +0200

    Changed name of parser expr test file

--- tests/parser/test_correct_expr.py
+++ tests/parser/test_correct_expr.py
@@ -1,4 +1,4 @@
-"""Correct token streams"""
+"""Correct token streams. """
 
 # pylint: disable=protected-access
 # pylint: disable=unidiomatic-typecheck
@@ -688,7 +688,7 @@ def test_all_comb_multi_op_before_and_after_unary_nested_unary_literal(
 
 @pytest.mark.parametrize("func", multi_parse_funcs)
 def test_unary_one_in_3_forms(func):
-    """ -1 * (-1) / -(1)"""
+    """-1 * (-1) / -(1)"""
     tokens = [
         Token(TokenType.MINUS, position=(1, 1)),
         Token(TokenType.INT_LITERAL, 1, position=(1, 2)),
@@ -707,8 +707,8 @@ def test_unary_one_in_3_forms(func):
     parser = Parser(lexer)
     result = func(parser)
     expected = MultiExpr(
-        [UnaryExpr(IntLiteral(1)), UnaryExpr(IntLiteral(1)),UnaryExpr(IntLiteral(1))],
-        ['*','/'],
+        [UnaryExpr(IntLiteral(1)), UnaryExpr(IntLiteral(1)), UnaryExpr(IntLiteral(1))],
+        ["*", "/"],
     )
     assert result == expected
 
@@ -1063,22 +1063,22 @@ def test_add_expr_nested_chain_start_from_left(func):
     assert result == expected
 
 
-
 rel_operators = [
-    (TokenType.LESS, '<'),
-    (TokenType.LESS_EQUAL, '<='),
-    (TokenType.EQUAL, '=='),
-    (TokenType.INEQUAL, '!='),
-    (TokenType.GREATER, '>'),
-    (TokenType.GREATER_EQUAL, '>='),
+    (TokenType.LESS, "<"),
+    (TokenType.LESS_EQUAL, "<="),
+    (TokenType.EQUAL, "=="),
+    (TokenType.INEQUAL, "!="),
+    (TokenType.GREATER, ">"),
+    (TokenType.GREATER_EQUAL, ">="),
 ]
 
 rel_parse_funcs = [
     Parser._parse_rel_expr,
     Parser._parse_logical_and_expr,
     Parser._parse_logical_or_expr,
-    Parser._parse_expr
+    Parser._parse_expr,
 ]
+
 
 @pytest.mark.parametrize("rel_token_type,rel_str", rel_operators)
 @pytest.mark.parametrize("func", rel_parse_funcs)
@@ -1099,8 +1099,8 @@ def test_rel_literals(func, rel_token_type, rel_str):
 
 @pytest.mark.parametrize("rel_token_type,rel_str", rel_operators)
 @pytest.mark.parametrize("func", rel_parse_funcs)
-def test_rel_(func,rel_token_type,rel_str):
-    """1+2*-3 rel_op 4""" # TODO after adding position to AST then two-letter rel operators will not pass
+def test_rel_(func, rel_token_type, rel_str):
+    """1+2*-3 rel_op 4"""  # TODO after adding position to AST then two-letter rel operators will not pass
     tokens = [
         Token(TokenType.INT_LITERAL, 1, position=(1, 1)),
         Token(TokenType.PLUS, position=(1, 2)),
@@ -1115,14 +1115,23 @@ def test_rel_(func,rel_token_type,rel_str):
     lexer = TokenProvider(None, tokens)
     parser = Parser(lexer)
     result = func(parser)
-    expected = RelationExpr(AddExpr([IntLiteral(1), MultiExpr([IntLiteral(2), UnaryExpr(IntLiteral(3))], ['*'])], ['+']), IntLiteral(4), rel_str)
+    expected = RelationExpr(
+        AddExpr(
+            [
+                IntLiteral(1),
+                MultiExpr([IntLiteral(2), UnaryExpr(IntLiteral(3))], ["*"]),
+            ],
+            ["+"],
+        ),
+        IntLiteral(4),
+        rel_str,
+    )
     assert result == expected
-
 
 
 @pytest.mark.parametrize("rel_token_type,rel_str", rel_operators)
 @pytest.mark.parametrize("func", rel_parse_funcs)
-def test_rel_sum_beofre_multi(func,rel_token_type,rel_str):
+def test_rel_sum_beofre_multi(func, rel_token_type, rel_str):
     """(1+2)*-3 rel_op 4"""
     tokens = [
         Token(TokenType.LEFT_BRACKET, position=(1, 1)),
@@ -1140,12 +1149,20 @@ def test_rel_sum_beofre_multi(func,rel_token_type,rel_str):
     lexer = TokenProvider(None, tokens)
     parser = Parser(lexer)
     result = func(parser)
-    expected = RelationExpr(MultiExpr([AddExpr([IntLiteral(1), IntLiteral(2)], ['+']), UnaryExpr(IntLiteral(3))], ['*']), IntLiteral(4), rel_str)
+    expected = RelationExpr(
+        MultiExpr(
+            [AddExpr([IntLiteral(1), IntLiteral(2)], ["+"]), UnaryExpr(IntLiteral(3))],
+            ["*"],
+        ),
+        IntLiteral(4),
+        rel_str,
+    )
     assert result == expected
+
 
 @pytest.mark.parametrize("rel_token_type,rel_str", rel_operators)
 @pytest.mark.parametrize("func", rel_parse_funcs)
-def test_rel_not_correct_relation_negation(func,rel_token_type,rel_str):
+def test_rel_not_correct_relation_negation(func, rel_token_type, rel_str):
     """- 1<2"""
     tokens = [
         Token(TokenType.MINUS, position=(1, 1)),
@@ -1160,9 +1177,10 @@ def test_rel_not_correct_relation_negation(func,rel_token_type,rel_str):
     expected = RelationExpr(UnaryExpr(IntLiteral(1)), IntLiteral(2), rel_str)
     assert result == expected
 
+
 @pytest.mark.parametrize("rel_token_type,rel_str", rel_operators)
 @pytest.mark.parametrize("func", rel_parse_funcs)
-def test_rel_correct_relation_negation(func,rel_token_type,rel_str):
+def test_rel_correct_relation_negation(func, rel_token_type, rel_str):
     """- (1<2)"""
     tokens = [
         Token(TokenType.MINUS, position=(1, 1)),
@@ -1182,7 +1200,7 @@ def test_rel_correct_relation_negation(func,rel_token_type,rel_str):
 
 @pytest.mark.parametrize("rel_token_type,rel_str", rel_operators)
 @pytest.mark.parametrize("func", rel_parse_funcs)
-def test_rel_negation_right_is_more_complex_than_left(func,rel_token_type,rel_str):
+def test_rel_negation_right_is_more_complex_than_left(func, rel_token_type, rel_str):
     """-(-4 < -1+-2/-3)"""
     tokens = [
         Token(TokenType.MINUS, position=(1, 1)),
@@ -1204,15 +1222,30 @@ def test_rel_negation_right_is_more_complex_than_left(func,rel_token_type,rel_st
     lexer = TokenProvider(None, tokens)
     parser = Parser(lexer)
     result = func(parser)
-    expected = UnaryExpr(RelationExpr(UnaryExpr(IntLiteral(4)), AddExpr([UnaryExpr(IntLiteral(1)), MultiExpr([UnaryExpr(IntLiteral(2)), UnaryExpr(IntLiteral(3))], ['/'])], ['+']), rel_str))
+    expected = UnaryExpr(
+        RelationExpr(
+            UnaryExpr(IntLiteral(4)),
+            AddExpr(
+                [
+                    UnaryExpr(IntLiteral(1)),
+                    MultiExpr(
+                        [UnaryExpr(IntLiteral(2)), UnaryExpr(IntLiteral(3))], ["/"]
+                    ),
+                ],
+                ["+"],
+            ),
+            rel_str,
+        )
+    )
     assert result == expected
 
 
 and_parse_funcs = [
     Parser._parse_logical_and_expr,
     Parser._parse_logical_or_expr,
-    Parser._parse_expr
+    Parser._parse_expr,
 ]
+
 
 @pytest.mark.parametrize("func", and_parse_funcs)
 def test_and_two_literals(func):
@@ -1229,6 +1262,7 @@ def test_and_two_literals(func):
     expected = AndExpr([IntLiteral(1), IntLiteral(2)])
     assert result == expected
 
+
 @pytest.mark.parametrize("func", and_parse_funcs)
 def test_and_three_literals(func):
     """1 & 2 & 0"""
@@ -1244,4 +1278,261 @@ def test_and_three_literals(func):
     parser = Parser(lexer)
     result = func(parser)
     expected = AndExpr([IntLiteral(1), IntLiteral(2), IntLiteral(0)])
+    assert result == expected
+
+
+or_parse_funcs = [Parser._parse_logical_or_expr, Parser._parse_expr]
+
+
+@pytest.mark.parametrize("func", or_parse_funcs)
+def test_or_two_literals(func):
+    """1 | 2"""
+    tokens = [
+        Token(TokenType.INT_LITERAL, 1, position=(1, 1)),
+        Token(TokenType.OR, position=(1, 3)),
+        Token(TokenType.INT_LITERAL, 2, position=(1, 5)),
+    ]
+
+    lexer = TokenProvider(None, tokens)
+    parser = Parser(lexer)
+    result = func(parser)
+    expected = OrExpr([IntLiteral(1), IntLiteral(2)])
+    assert result == expected
+
+
+@pytest.mark.parametrize("func", or_parse_funcs)
+def test_or_three_literals(func):
+    """1 | 2 | 0"""
+    tokens = [
+        Token(TokenType.INT_LITERAL, 1, position=(1, 1)),
+        Token(TokenType.OR, position=(1, 3)),
+        Token(TokenType.INT_LITERAL, 2, position=(1, 5)),
+        Token(TokenType.OR, position=(1, 7)),
+        Token(TokenType.INT_LITERAL, 0, position=(1, 9)),
+    ]
+
+    lexer = TokenProvider(None, tokens)
+    parser = Parser(lexer)
+    result = func(parser)
+    expected = OrExpr([IntLiteral(1), IntLiteral(2), IntLiteral(0)])
+    assert result == expected
+
+
+@pytest.mark.parametrize("func", or_parse_funcs)
+def test_or_precedance_of_all_supported_operators(func):
+    """1|2&3<4+5*-6|0"""
+    tokens = [
+        Token(TokenType.INT_LITERAL, 1, position=(1, 1)),
+        Token(TokenType.OR, position=(1, 2)),
+        Token(TokenType.INT_LITERAL, 2, position=(1, 3)),
+        Token(TokenType.AND, position=(1, 4)),
+        Token(TokenType.INT_LITERAL, 3, position=(1, 5)),
+        Token(TokenType.LESS, position=(1, 6)),
+        Token(TokenType.INT_LITERAL, 4, position=(1, 7)),
+        Token(TokenType.PLUS, position=(1, 8)),
+        Token(TokenType.INT_LITERAL, 5, position=(1, 9)),
+        Token(TokenType.TIMES, position=(1, 10)),
+        Token(TokenType.MINUS, position=(1, 11)),
+        Token(TokenType.INT_LITERAL, 6, position=(1, 12)),
+        Token(TokenType.OR, position=(1, 13)),
+        Token(TokenType.INT_LITERAL, 0, position=(1, 14)),
+    ]
+
+    lexer = TokenProvider(None, tokens)
+    parser = Parser(lexer)
+    result = func(parser)
+    expected = OrExpr(
+        [
+            IntLiteral(1),
+            AndExpr(
+                [
+                    IntLiteral(2),
+                    RelationExpr(
+                        IntLiteral(3),
+                        AddExpr(
+                            [
+                                IntLiteral(4),
+                                MultiExpr(
+                                    [IntLiteral(5), UnaryExpr(IntLiteral(6))], ["*"]
+                                ),
+                            ],
+                            ["+"],
+                        ),
+                        "<",
+                    ),
+                ]
+            ),
+            IntLiteral(0),
+        ]
+    )
+    assert result == expected
+
+
+@pytest.mark.parametrize("func", or_parse_funcs)
+def test_or_complex(func):
+    """-(1 < 2 & 3 >= 4) |
+    5 != 6 & (7 == 8 | 9 > 10) & ((11 <= 12 | 13 > 14) & -15 < 16 | 17 == 18 & -19 >= 20) |
+    21 != 22 & (23 < 24 | 25 >= 26) |
+    -(27 == 28) & (29 != 30 | 31 <= 32)"""
+    tokens = [
+        Token(TokenType.MINUS, position=(1, 1)),
+        Token(TokenType.LEFT_BRACKET, position=(1, 2)),
+        Token(TokenType.INT_LITERAL, 1, position=(1, 3)),
+        Token(TokenType.LESS, position=(1, 5)),
+        Token(TokenType.INT_LITERAL, 2, position=(1, 7)),
+        Token(TokenType.AND, position=(1, 9)),
+        Token(TokenType.INT_LITERAL, 3, position=(1, 11)),
+        Token(TokenType.GREATER_EQUAL, position=(1, 13)),
+        Token(TokenType.INT_LITERAL, 4, position=(1, 16)),
+        Token(TokenType.RIGHT_BRACKET, position=(1, 17)),
+        Token(TokenType.OR, position=(1, 19)),
+        Token(TokenType.INT_LITERAL, 5, position=(2, 5)),
+        Token(TokenType.INEQUAL, position=(2, 7)),
+        Token(TokenType.INT_LITERAL, 6, position=(2, 10)),
+        Token(TokenType.AND, position=(2, 12)),
+        Token(TokenType.LEFT_BRACKET, position=(2, 14)),
+        Token(TokenType.INT_LITERAL, 7, position=(2, 15)),
+        Token(TokenType.EQUAL, position=(2, 17)),
+        Token(TokenType.INT_LITERAL, 8, position=(2, 20)),
+        Token(TokenType.OR, position=(2, 22)),
+        Token(TokenType.INT_LITERAL, 9, position=(2, 24)),
+        Token(TokenType.GREATER, position=(2, 26)),
+        Token(TokenType.INT_LITERAL, 10, position=(2, 28)),
+        Token(TokenType.RIGHT_BRACKET, position=(2, 30)),
+        Token(TokenType.AND, position=(2, 32)),
+        Token(TokenType.LEFT_BRACKET, position=(2, 34)),
+        Token(TokenType.LEFT_BRACKET, position=(2, 35)),
+        Token(TokenType.INT_LITERAL, 11, position=(2, 36)),
+        Token(TokenType.LESS_EQUAL, position=(2, 39)),
+        Token(TokenType.INT_LITERAL, 12, position=(2, 42)),
+        Token(TokenType.OR, position=(2, 45)),
+        Token(TokenType.INT_LITERAL, 13, position=(2, 47)),
+        Token(TokenType.GREATER, position=(2, 50)),
+        Token(TokenType.INT_LITERAL, 14, position=(2, 52)),
+        Token(TokenType.RIGHT_BRACKET, position=(2, 54)),
+        Token(TokenType.AND, position=(2, 56)),
+        Token(TokenType.MINUS, position=(2, 58)),
+        Token(TokenType.INT_LITERAL, 15, position=(2, 59)),
+        Token(TokenType.LESS, position=(2, 62)),
+        Token(TokenType.INT_LITERAL, 16, position=(2, 64)),
+        Token(TokenType.OR, position=(2, 67)),
+        Token(TokenType.INT_LITERAL, 17, position=(2, 69)),
+        Token(TokenType.EQUAL, position=(2, 72)),
+        Token(TokenType.INT_LITERAL, 18, position=(2, 75)),
+        Token(TokenType.AND, position=(2, 78)),
+        Token(TokenType.MINUS, position=(2, 80)),
+        Token(TokenType.INT_LITERAL, 19, position=(2, 81)),
+        Token(TokenType.GREATER_EQUAL, position=(2, 84)),
+        Token(TokenType.INT_LITERAL, 20, position=(2, 87)),
+        Token(TokenType.RIGHT_BRACKET, position=(2, 89)),
+        Token(TokenType.OR, position=(2, 91)),
+        Token(TokenType.INT_LITERAL, 21, position=(3, 5)),
+        Token(TokenType.INEQUAL, position=(3, 8)),
+        Token(TokenType.INT_LITERAL, 22, position=(3, 11)),
+        Token(TokenType.AND, position=(3, 14)),
+        Token(TokenType.LEFT_BRACKET, position=(3, 16)),
+        Token(TokenType.INT_LITERAL, 23, position=(3, 17)),
+        Token(TokenType.LESS, position=(3, 20)),
+        Token(TokenType.INT_LITERAL, 24, position=(3, 22)),
+        Token(TokenType.OR, position=(3, 25)),
+        Token(TokenType.INT_LITERAL, 25, position=(3, 27)),
+        Token(TokenType.GREATER_EQUAL, position=(3, 30)),
+        Token(TokenType.INT_LITERAL, 26, position=(3, 33)),
+        Token(TokenType.RIGHT_BRACKET, position=(3, 35)),
+        Token(TokenType.OR, position=(3, 37)),
+        Token(TokenType.MINUS, position=(4, 5)),
+        Token(TokenType.LEFT_BRACKET, position=(4, 6)),
+        Token(TokenType.INT_LITERAL, 27, position=(4, 7)),
+        Token(TokenType.EQUAL, position=(4, 10)),
+        Token(TokenType.INT_LITERAL, 28, position=(4, 13)),
+        Token(TokenType.RIGHT_BRACKET, position=(4, 15)),
+        Token(TokenType.AND, position=(4, 17)),
+        Token(TokenType.LEFT_BRACKET, position=(4, 19)),
+        Token(TokenType.INT_LITERAL, 29, position=(4, 20)),
+        Token(TokenType.INEQUAL, position=(4, 23)),
+        Token(TokenType.INT_LITERAL, 30, position=(4, 26)),
+        Token(TokenType.OR, position=(4, 29)),
+        Token(TokenType.INT_LITERAL, 31, position=(4, 31)),
+        Token(TokenType.LESS_EQUAL, position=(4, 34)),
+        Token(TokenType.INT_LITERAL, 32, position=(4, 37)),
+        Token(TokenType.RIGHT_BRACKET, position=(4, 39)),
+    ]
+
+    lexer = TokenProvider(None, tokens)
+    parser = Parser(lexer)
+    result = func(parser)
+    expected = OrExpr(
+        [
+            UnaryExpr(
+                AndExpr(
+                    [
+                        RelationExpr(IntLiteral(1), IntLiteral(2), "<"),
+                        RelationExpr(IntLiteral(3), IntLiteral(4), ">="),
+                    ]
+                )
+            ),
+            AndExpr(
+                [
+                    RelationExpr(IntLiteral(5), IntLiteral(6), "!="),
+                    OrExpr(
+                        [
+                            RelationExpr(IntLiteral(7), IntLiteral(8), "=="),
+                            RelationExpr(IntLiteral(9), IntLiteral(10), ">"),
+                        ]
+                    ),
+                    OrExpr(
+                        [
+                            AndExpr(
+                                [
+                                    OrExpr(
+                                        [
+                                            RelationExpr(
+                                                IntLiteral(11), IntLiteral(12), "<="
+                                            ),
+                                            RelationExpr(
+                                                IntLiteral(13), IntLiteral(14), ">"
+                                            ),
+                                        ]
+                                    ),
+                                    RelationExpr(
+                                        UnaryExpr(IntLiteral(15)), IntLiteral(16), "<"
+                                    ),
+                                ]
+                            ),
+                            AndExpr(
+                                [
+                                    RelationExpr(IntLiteral(17), IntLiteral(18), "=="),
+                                    RelationExpr(
+                                        UnaryExpr(IntLiteral(19)), IntLiteral(20), ">="
+                                    ),
+                                ]
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            AndExpr(
+                [
+                    RelationExpr(IntLiteral(21), IntLiteral(22), "!="),
+                    OrExpr(
+                        [
+                            RelationExpr(IntLiteral(23), IntLiteral(24), "<"),
+                            RelationExpr(IntLiteral(25), IntLiteral(26), ">="),
+                        ]
+                    ),
+                ]
+            ),
+            AndExpr(
+                [
+                    UnaryExpr(RelationExpr(IntLiteral(27), IntLiteral(28), "==")),
+                    OrExpr(
+                        [
+                            RelationExpr(IntLiteral(29), IntLiteral(30), "!="),
+                            RelationExpr(IntLiteral(31), IntLiteral(32), "<="),
+                        ]
+                    ),
+                ]
+            ),
+        ]
+    )
     assert result == expected
