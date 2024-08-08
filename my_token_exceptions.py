86ae77de3b818dd32c762f7ba0d4fe5645c94337
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-08-08 19:50:05 +0200

    Added exceptions to lexer exceptions

--- my_token_exceptions.py
+++ my_token_exceptions.py
@@ -14,8 +14,13 @@ class MyTokenException(Exception):
         return f"{self.__class__.__name__}: row: {self.row}, column: {self.col}, {self.__doc__}"
 
 
+class UnrecognisedStartOfToken(MyTokenException):
+    """Encountered character that no token starts with!"""
+
+
 class IdentifierCanNotStartWithUnderscore(MyTokenException):
     """While building new token first char was '_'. Identifiers can not start with '_'!"""
+
 
 class StringLiteralError(MyTokenException):
     """Error when building string literal token"""
@@ -25,12 +30,16 @@ class StringLiteralNotEnded(StringLiteralError):
     """Got newline or end of text while building string literal"""
 
 
+class UseOfQuotationMarksIsInvalid(StringLiteralError):
+    """Use apostrophe (') instead of quotattion mark (") to mark boundaries of string literal."""
+
+
 class EscapingWrongChar(StringLiteralError):
     """Valid escaping chars are \\, t, n, '"""
 
+
 class EscapingEOT(StringLiteralError):
     """Received end of text instead of \\, t, n, '"""
-
 
 
 class ExclamationMarkError(MyTokenException):
@@ -40,34 +49,42 @@ class ExclamationMarkError(MyTokenException):
 class NumberError(MyTokenException):
     """Error when building number token"""
 
+
 class NumberLiteralTooBig(NumberError):
     """Number literal has a limit"""
 
+
 class NumberLiteralTooManyChars(NumberError):
     """Number literals have a limit. Sum of digits of integer part and fractional part can not exceed"""
-    def __init__(self, position: PositionType = (None, None), limit : int = 30):
+
+    def __init__(self, position: PositionType = (None, None), limit: int = 30):
         self.row, self.col = position
         self.limit = limit
         super().__init__()
 
     def __str__(self):
         return f"{self.__class__.__name__}: row: {self.row}, column: {self.col}, {self.__doc__}: {self.limit}"
+
+
 class IntLiteralTooBig(NumberLiteralTooManyChars):
     """Int char limit is"""
+
 
 class FloatLiteralTooBig(NumberLiteralTooManyChars):
     """Float char limit is"""
 
+
 class DigitRequiredAfterDot(NumberError):
     """When building float literal, there has to be at least one digit after a dot"""
 
-class InvalidCharInIdentifier(MyTokenException):
-    """Identifier body can only consist of letterz a-z, A-Z, digits and _. """
 
-# TODO add int and float limits to doc strings...
+class InvalidCharInIdentifier(MyTokenException):
+    """Identifier body can only consist of letterz a-z, A-Z, digits and _."""
+
 
 class PrecidingZerosError(NumberError):
     """Putting additional zeros to the left of number literal is not allowed. 0 is ok, so is 0.1 so is 0.0001. 01 is  not. 00.1 is not"""
+
 
 class IdentifierTooLong(MyTokenException):
     """self explanatory"""
