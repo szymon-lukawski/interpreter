8d9b7c963e57116375dbe787ce219889f629f58f
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-08-08 19:50:33 +0200

    Added expected different token parser exeption

--- parser_exceptions.py
+++ parser_exceptions.py
@@ -1,9 +1,10 @@
 """Parser exceptions"""
 
 from my_token import PositionType
+from token_type import TokenType
 
 class ParserException(Exception):
-    """Base class for all Lexer related errors"""
+    """Base class for all Parser related errors"""
 
     def __init__(self, position: PositionType = (None, None)):
         self.row, self.col = position
@@ -12,3 +13,16 @@ class ParserException(Exception):
     def __str__(self):
         return f"{self.__class__.__name__}: row: {self.row}, column: {self.col}, {self.__doc__}"
 
+
+
+class ExpectedDifferentToken(ParserException):
+    """Got different token than expected"""
+    def __init__(self, position: PositionType = (None, None), msg = None):
+        self.row, self.col = position
+        self.msg = msg
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: row: {self.row}, column: {self.col}, {self.msg}"
+
+class PatternNotRecognised(ParserException):
+    """While building new statement, starting pattern was not recognised."""
