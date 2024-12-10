59d6864865be3a558f88a6a48bec723a86233528
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-12-10 23:26:27 +0100

    feat: Added rest of dispatch function definitions for all types

--- operations.py
+++ operations.py
@@ -5,9 +5,51 @@ from multipledispatch import dispatch
 def add(left, right):
     return add(left.value, right.value)
 
+
+@dispatch(StructValue, BuiltInValue)
+def add(left, right):
+    raise RuntimeError("Can not add struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def add(left, right):
+    return add(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def add(left, right):
+    raise RuntimeError("Can not add built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def add(left, right):
+    raise RuntimeError("Can not add structs")
+
+@dispatch(VariantValue, StructValue)
+def add(left, right):
+    return add(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def add(left, right):
+    return add(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def add(left, right):
+    return add(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
+def add(left, right):
+    return add(left.value, right.value)
+
 @dispatch(int, int)
 def add(left, right):
     return BuiltInValue('int', left + right)
+
+@dispatch(float, int)
+def add(left, right):
+    return BuiltInValue('float', left + float(right))
+
+@dispatch(str, int)
+def add(left, right):
+    return BuiltInValue('str', left + str(right))
+
 
 
 @dispatch(object, object)
@@ -17,6 +59,38 @@ def add(left, right):
 # -------------------
 
 @dispatch(BuiltInValue, BuiltInValue)
+def sub(left, right):
+    return sub(left.value, right.value)
+
+@dispatch(StructValue, BuiltInValue)
+def sub(left, right):
+    raise RuntimeError("Can not sub struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def sub(left, right):
+    return sub(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def sub(left, right):
+    raise RuntimeError("Can not sub built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def sub(left, right):
+    raise RuntimeError("Can not sub structs")
+
+@dispatch(VariantValue, StructValue)
+def sub(left, right):
+    return sub(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def sub(left, right):
+    return sub(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def sub(left, right):
+    return sub(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
 def sub(left, right):
     return sub(left.value, right.value)
 
@@ -35,6 +109,38 @@ def sub(left, right):
 def mul(left, right):
     return mul(left.value, right.value)
 
+@dispatch(StructValue, BuiltInValue)
+def add(left, right):
+    raise RuntimeError("Can not mul struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def mul(left, right):
+    return mul(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def mul(left, right):
+    raise RuntimeError("Can not mul built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def mul(left, right):
+    raise RuntimeError("Can not mul structs")
+
+@dispatch(VariantValue, StructValue)
+def mul(left, right):
+    return mul(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def mul(left, right):
+    return mul(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def mul(left, right):
+    return mul(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
+def mul(left, right):
+    return add(left.value, right.value)
+
 @dispatch(int, int)
 def mul(left, right):
     return BuiltInValue('int', left * right)
@@ -47,6 +153,38 @@ def mul(left, right):
 # -------------------------
 
 @dispatch(BuiltInValue, BuiltInValue)
+def div(left, right):
+    return div(left.value, right.value)
+
+@dispatch(StructValue, BuiltInValue)
+def div(left, right):
+    raise RuntimeError("Can not div struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def div(left, right):
+    return div(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def div(left, right):
+    raise RuntimeError("Can not div built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def div(left, right):
+    raise RuntimeError("Can not div structs")
+
+@dispatch(VariantValue, StructValue)
+def div(left, right):
+    return div(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def div(left, right):
+    return div(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def div(left, right):
+    return div(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
 def div(left, right):
     return div(left.value, right.value)
 
@@ -67,6 +205,38 @@ def div(left, right):
 def eq(left, right):
     return eq(left.value, right.value)
 
+@dispatch(StructValue, BuiltInValue)
+def eq(left, right):
+    raise RuntimeError("Can not eq struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def eq(left, right):
+    return eq(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def eq(left, right):
+    raise RuntimeError("Can not eq built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def eq(left, right):
+    raise RuntimeError("Can not eq structs")
+
+@dispatch(VariantValue, StructValue)
+def eq(left, right):
+    return eq(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def eq(left, right):
+    return eq(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def eq(left, right):
+    return eq(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
+def eq(left, right):
+    return eq(left.value, right.value)
+
 @dispatch(int, int)
 def eq(left, right):
     return BuiltInValue('int', int(left == right))
@@ -81,6 +251,38 @@ def eq(left, right):
 
 
 @dispatch(BuiltInValue, BuiltInValue)
+def ieq(left, right):
+    return ieq(left.value, right.value)
+
+@dispatch(StructValue, BuiltInValue)
+def ieq(left, right):
+    raise RuntimeError("Can not ieq struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def ieq(left, right):
+    return ieq(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def ieq(left, right):
+    raise RuntimeError("Can not ieq built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def ieq(left, right):
+    raise RuntimeError("Can not ieq structs")
+
+@dispatch(VariantValue, StructValue)
+def ieq(left, right):
+    return ieq(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def ieq(left, right):
+    return ieq(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def ieq(left, right):
+    return ieq(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
 def ieq(left, right):
     return ieq(left.value, right.value)
 
@@ -100,6 +302,38 @@ def ieq(left, right):
 def lt(left, right):
     return lt(left.value, right.value)
 
+@dispatch(StructValue, BuiltInValue)
+def lt(left, right):
+    raise RuntimeError("Can not lt struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def lt(left, right):
+    return lt(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def lt(left, right):
+    raise RuntimeError("Can not lt built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def lt(left, right):
+    raise RuntimeError("Can not lt structs")
+
+@dispatch(VariantValue, StructValue)
+def lt(left, right):
+    return lt(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def lt(left, right):
+    return lt(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def lt(left, right):
+    return lt(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
+def lt(left, right):
+    return lt(left.value, right.value)
+
 @dispatch(int, int)
 def lt(left, right):
     return BuiltInValue('int', int(left < right))
@@ -113,6 +347,38 @@ def lt(left, right):
 
 
 @dispatch(BuiltInValue, BuiltInValue)
+def gt(left, right):
+    return gt(left.value, right.value)
+
+@dispatch(StructValue, BuiltInValue)
+def gt(left, right):
+    raise RuntimeError("Can not gt struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def gt(left, right):
+    return gt(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def gt(left, right):
+    raise RuntimeError("Can not gt built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def gt(left, right):
+    raise RuntimeError("Can not gt structs")
+
+@dispatch(VariantValue, StructValue)
+def gt(left, right):
+    return gt(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def gt(left, right):
+    return gt(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def gt(left, right):
+    return gt(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
 def gt(left, right):
     return gt(left.value, right.value)
 
@@ -132,6 +398,38 @@ def gt(left, right):
 def lteq(left, right):
     return lteq(left.value, right.value)
 
+@dispatch(StructValue, BuiltInValue)
+def lteq(left, right):
+    raise RuntimeError("Can not lteq struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def lteq(left, right):
+    return lteq(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def lteq(left, right):
+    raise RuntimeError("Can not lteq built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def lteq(left, right):
+    raise RuntimeError("Can not lteq structs")
+
+@dispatch(VariantValue, StructValue)
+def lteq(left, right):
+    return lteq(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def lteq(left, right):
+    return lteq(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def lteq(left, right):
+    return lteq(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
+def lteq(left, right):
+    return lteq(left.value, right.value)
+
 @dispatch(int, int)
 def lteq(left, right):
     return BuiltInValue('int', int(left <= right))
@@ -146,6 +444,38 @@ def lteq(left, right):
 
 
 @dispatch(BuiltInValue, BuiltInValue)
+def gteq(left, right):
+    return gteq(left.value, right.value)
+
+@dispatch(StructValue, BuiltInValue)
+def gteq(left, right):
+    raise RuntimeError("Can not gteq struct and built in")
+
+@dispatch(VariantValue, BuiltInValue)
+def gteq(left, right):
+    return gteq(left.value, right)
+
+@dispatch(BuiltInValue, StructValue)
+def gteq(left, right):
+    raise RuntimeError("Can not gteq built in and structs ")
+
+@dispatch(StructValue, StructValue)
+def gteq(left, right):
+    raise RuntimeError("Can not gteq structs")
+
+@dispatch(VariantValue, StructValue)
+def gteq(left, right):
+    return gteq(left.value, right)
+
+@dispatch(BuiltInValue, VariantValue)
+def gteq(left, right):
+    return gteq(left, right.value)
+
+@dispatch(StructValue, VariantValue)
+def gteq(left, right):
+    return gteq(left, right.value)
+
+@dispatch(VariantValue, VariantValue)
 def gteq(left, right):
     return gteq(left.value, right.value)
 
